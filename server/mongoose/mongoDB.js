426a8c2564c307bcece1a6cd50fc71d14539b12d
Ernestobolkin <ernest1047@gmail.com>
2022-01-21 16:50:16 +0200

    edit the url

--- server/mongoose/mongoDB.js
+++ server/mongoose/mongoDB.js
@@ -2,7 +2,7 @@ const { connect, model } = require("mongoose");
 const chalk = require("chalk");
 
 module.exports = connect(
-  "mongodb+srv://elzo:XhxnvNubdu@ertodatabase.ilvau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "mongodb+srv://elzo:XhxnvNubdu@ertodatabase.ilvau.mongodb.net/ertoDataBase?retryWrites=true&w=majority",
   () => {
     console.log(chalk.yellow("mongoDB connected"));
   },
