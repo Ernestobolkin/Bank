53f6dcf368a4bd0229f5114757846d553260c7a2
Ernestobolkin <ernest1047@gmail.com>
2022-01-21 16:37:28 +0200

    testing

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require("dotenv").config();
-const cors = require("cors");
 const apiRouter = require("./routes/routes")
 
 
